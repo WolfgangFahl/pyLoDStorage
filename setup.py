70f4fc086136033f36f9a54221e4fad255df124f
Wolfgang Fahl <wf@bitplan.com>
2020-09-10 09:04:48 +0200

    adds github workflows

---
+++ setup.py
@@ -0,0 +1,32 @@
+# ! important
+# see https://stackoverflow.com/a/27868004/1497139
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pylodstorage',
+    version='0.0.1',
+
+    packages=['storage',],
+    author='Wolfgang Fahl',
+    author='Somnath Rakshit',
+    author_email='wf@bitplan.com',
+    maintainer='Wolfgang Fahl',
+    url='https://github.com/WolfgangFahl/pyLodStorage',
+    license='Apache License',
+    description='python List of Dict (Table) Storage library',
+    classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
