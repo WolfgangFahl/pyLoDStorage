681357843253c82ee1a7843b31b4d9ed03c2e967
Wolfgang Fahl <wf@bitplan.com>
2020-09-12 07:25:14 +0200

    adds python3.6 support per pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pylodstorage',
-    version='0.0.6',
+    version='0.0.7',
 
     packages=['lodstorage',],
     author='Wolfgang Fahl',
@@ -29,6 +29,7 @@ setup(
     description='python List of Dict (Table) Storage library',
     classifiers=[
             'Programming Language :: Python',
+            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8'
     ],
