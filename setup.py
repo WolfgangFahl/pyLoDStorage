caa25efba8d858c1a78b656fc8f91f9ef2f6ae91
Wolfgang Fahl <wf@bitplan.com>
2020-09-21 18:09:28 +0200

    prepares release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pylodstorage',
-    version='0.0.10',
+    version='0.0.12',
 
     packages=['lodstorage',],
     author='Wolfgang Fahl',
