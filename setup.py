f8ac0722918bd79cfb4140ede23a2729b9c004fc
Wolfgang Fahl <wf@bitplan.com>
2021-07-12 10:45:08 +0200

    tries jumping to 0.0.37 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pylodstorage',
-    version='0.0.36',
+    version='0.0.37',
 
     packages=['lodstorage',],
     author='Wolfgang Fahl',
