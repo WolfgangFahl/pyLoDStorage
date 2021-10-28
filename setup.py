4a08e8472b93b7d4b97100caba17b5cc666dcfec
Wolfgang Fahl <wf@bitplan.com>
2021-10-28 13:09:34 +0200

    fixes #48

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pylodstorage',
-    version='0.0.74',
+    version='0.0.75',
 
     packages=['lodstorage',],
     author='Wolfgang Fahl',
