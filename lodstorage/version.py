bb16954897c6a0b4b4842581c546d2c7e6bbac51
Wolfgang Fahl <wf@bitplan.com>
2022-03-14 16:27:22 +0100

    fixes #77

--- lodstorage/version.py
+++ lodstorage/version.py
@@ -9,8 +9,8 @@ class Version(object):
     Version handling for ConferenceCorpus
     '''
     name="pyLodStorage"
-    version='0.1.18'
+    version='0.1.19'
     date = '2020-09-10'
-    updated = '2022-03-11'
+    updated = '2022-03-14'
 
         
