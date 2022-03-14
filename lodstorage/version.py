eee735c1fae0dda8e9ee7a67cd5a30047f8dffd5
Wolfgang Fahl <wf@bitplan.com>
2022-03-14 18:46:41 +0100

    tries upgrading to Sparqlwraper 2.0.0

--- lodstorage/version.py
+++ lodstorage/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for ConferenceCorpus
     '''
     name="pyLodStorage"
-    version='0.1.19'
+    version='0.2.0'
     date = '2020-09-10'
     updated = '2022-03-14'
 
