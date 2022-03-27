6a327ce7ea5fce94b272ac6424b19daca45e6be9
Wolfgang Fahl <wf@bitplan.com>
2022-03-27 17:28:15 +0200

    adds openlinksw endpoint and fixes president nickname query

--- lodstorage/version.py
+++ lodstorage/version.py
@@ -9,6 +9,6 @@ class Version(object):
     Version handling for pyLoDStorage
     '''
     name="pylodstorage"
-    version='0.2.0'
+    version='0.2.1'
     date = '2020-09-10'
-    updated = '2022-03-24'
+    updated = '2022-03-27'
