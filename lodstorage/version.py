3d03bca9e13c04c2584c8e835b9c9106c1b7d5a2
Wolfgang Fahl <wf@bitplan.com>
2023-03-28 09:13:43 +0200

    adds dblp-plus to default endpoints

--- lodstorage/version.py
+++ lodstorage/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyLoDStorage
     '''
     name="pylodstorage"
-    version='0.4.10'
+    version='0.4.11'
     date = '2020-09-10'
-    updated = '2023-03-21'
+    updated = '2023-03-28'
     description='python List of Dict (Table) Storage library'
