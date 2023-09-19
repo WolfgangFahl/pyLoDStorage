85a57a807b1e19c938bb6c8b0985a67497919a27
Wolfgang Fahl <wf@bitplan.com>
2023-09-19 14:52:57 +0200

    fixes #116

--- lodstorage/version.py
+++ lodstorage/version.py
@@ -3,13 +3,14 @@ Created on 2022-03-06
 
 @author: wf
 '''
+import lodstorage
 
 class Version(object):
     '''
     Version handling for pyLoDStorage
     '''
     name="pylodstorage"
-    version='0.4.11'
+    version=lodstorage.__version__
     date = '2020-09-10'
     updated = '2023-03-28'
     description='python List of Dict (Table) Storage library'
