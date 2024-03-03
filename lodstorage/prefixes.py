859dc7833cf9dd95267a91236bdcbee3a6098a2d
Wolfgang Fahl <wf@bitplan.com>
2024-03-03 08:16:25 +0100

    tries fixing jena script

--- lodstorage/prefixes.py
+++ lodstorage/prefixes.py
@@ -7,14 +7,43 @@ Created on 2024-03-02
 
 class Prefixes:
     """
-    handle standard SPARQL Prefixes
+    Handles the generation of standard SPARQL prefix declarations for queries.
+    This utility class simplifies the inclusion of common prefixes used in SPARQL
+    queries by providing a method to generate the necessary PREFIX lines based on
+    a list of prefix keys.
+
+    The class supports a wide range of prefixes relevant to Wikidata and general RDF/SPARQL
+    usage, including RDF, RDFS, Wikibase, Schema.org, and more. It aims to reduce redundancy
+    and improve clarity in SPARQL query construction by centralizing prefix management.
+
+    Attributes:
+        None
+
+    Methods:
+        getPrefixes(prefixes): Generates SPARQL PREFIX lines for a given list of prefix keys.
     """
 
     @classmethod
     def getPrefixes(
         cls, prefixes=["rdf", "rdfs", "schema", "wd", "wdt", "wikibase", "xsd"]
     ) -> str:
-        """ 
+        """Generates SPARQL PREFIX lines for a given list of prefix keys.
+
+        This method looks up URIs for the specified prefixes from a predefined map and constructs
+        PREFIX lines suitable for inclusion at the beginning of a SPARQL query. It allows for easy
+        and flexible specification of the prefixes needed for a particular query.
+
+        Args:
+            prefixes (list of str): A list of prefix keys for which PREFIX lines should be generated.
+                Defaults to a common set of prefixes used in Wikidata queries.
+
+        Returns:
+            str: A string containing the SPARQL PREFIX lines for the specified prefixes, each ending
+                with a newline character. If a prefix key is not recognized, it is ignored.
+
+        Example:
+            >>> Prefixes.getPrefixes(["wd", "wdt"])
+            'PREFIX wd: <http://www.wikidata.org/entity/>\nPREFIX wdt: <http://www.wikidata.org/prop/direct/>\n'
         """
         prefixMap = {
             "bd": "<http://www.bigdata.com/rdf#>",
