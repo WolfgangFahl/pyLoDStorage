63b16f75db83aebf33b7f527f22cc63b25035e81
Wolfgang Fahl <wf@bitplan.com>
2024-08-02 14:36:17 +0200

    release commit

--- lodstorage/version.py
+++ lodstorage/version.py
@@ -14,5 +14,5 @@ class Version(object):
     name = "pylodstorage"
     version = lodstorage.__version__
     date = "2020-09-10"
-    updated = "2024-07-02"
+    updated = "2024-08-02"
     description = "python List of Dict (Table) Storage library"
